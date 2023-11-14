29383343abda3431b8fe7fefcc0fff79c23ce8b9
kimseulgi <ksk177985@gmail.com>
2023-11-14 15:56:21 +0900

    wallet

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,18 @@
 import 'package:flutter/material.dart';
 
-class Player {
-  String? name;
-
-  Player({required this.name});
-}
-
 void main() {
-  var ksg = Player(name: "ksg");
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    //Android: MaterialApp, IOS: CupertinoApp
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-          title: Center(
-            child: Text('Hello flutter!'),
-          ),
-        ),
-        body: Center(
-          child: Text('Hello world!'),
-        ),
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Container(),
       ),
     );
   }
